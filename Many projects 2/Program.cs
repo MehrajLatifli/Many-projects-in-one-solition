d325d7c950a84b05f261078c540b97741a71f892
Mehrac Lətifli <meracletifli@gmail.com>
2021-12-02 02:18:09 +0400

    many project in one solition

---
+++ Many projects 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Many_projects_2
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            // First time run second project if you did make changes in the second project. Because subsequent changes are not remembered in second project when run first project.  
+
+            Console.WriteLine("Project 2");
+
+            var v = System.IO.Path.Combine("../../../Many projects/bin/Debug", "Many projects.exe");  //  ../../../ => Debug => bin => Many Projects 2
+
+            MessageBox.Show($"Project 1: {v}");
+
+            Process.Start($"{v}");
+
+            Environment.Exit(1);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Many projects/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Many_projects
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //String appStartPath = System.IO.Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
+            //Process aProcess = new Process();
+            //aProcess.StartInfo.FileName = appStartPath;// not sure if it's FilePath, just look through the properties
+            //aProcess.Start();
+
+            var v =System.IO.Path.Combine("../../../Many projects 2/bin/Debug", "Many projects 2.exe");  //  ../../../ => Debug => bin => Many Projects
+
+            MessageBox.Show($"Project 2: {v}");
+
+            Process.Start($"{v}");
+
+            Environment.Exit(1);
+
+        }
+    }
+}
